e7025ec871113807a3c95588c6994f1b5e1338d9
zozors <romance.dawn1989@gmail.com>
2022-05-20 20:06:33 +0900

    factoryBot導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,6 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:last_name,:first_name,:last_name_katakane,:first_name_katakana,:birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:last_name,:first_name,:last_name_katakana,:first_name_katakana,:birthday])
   end
 end
