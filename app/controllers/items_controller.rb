b07c2af4f7b2df9128e74b2da61f8d2de4118745
zozors <romance.dawn1989@gmail.com>
2022-05-26 15:33:33 +0900

    edit,update機能追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,16 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+      redirect_to item_path
+  end
+
   private
 
   def item_params
