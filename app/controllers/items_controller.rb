9cf1acc2df17d49fe1b3a32bbbf02d820421ba5d
zozors <romance.dawn1989@gmail.com>
2022-05-27 15:30:51 +0900

    destroyアクション記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show ]
-  before_action :set_item, only: [:edit, :show, :update]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
   before_action :move_to_index, only: [:edit]
   
   
@@ -35,6 +35,13 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
+
+
   private
 
   def item_params
